9c0b53d61cce06483cb1e1e6e207c353c39dc494
Jakub Kołuda <jko01@outlook.com>
2022-07-20 21:07:28 +0200

    change main.js add showUsers()

--- main.js
+++ main.js
@@ -4,20 +4,35 @@ const getUsers = (e) => {
     const quantity = document.querySelector(".generator__input").value
 
     const url = `https://randomuser.me/api/?results=${quantity}&gender=${selectGender === "both" ? "male,female" : selectGender}`;
-    
+
+
     fetch(url)
-    .then(response => {
-    if(response.status !== 200) {
-        throw Error("status uneaqual 200");
-    } else return response.json();
-    })
-    .then (json => {
-        console.log(json.results);
-        
-    })
-    .catch(error => {console.log(error);
-    })
+        .then(response => {
+            if (response.status !== 200) {
+                throw Error("status uneaqual 200");
+            } else return response.json();
+        })
+        .then(json => {
+            showUsers(json.results);
+
+        })
+        .catch(error => {
+            console.log(error);
+        })
 }
 
+const showUsers = (results) => {
+    // console.log(results);
+    const usersList = document.querySelector(".users-list");
+    results.forEach(result => {
+        console.log(result);
+        const item = document.createElement("div");
+        item.className = "user";
+        name.textContent = `${result.name.title}  ${result.name.first}  ${result.name.last}`;
+        item.innerHTML = `<img src="${result.picture.medium}" class="user__img"> <span class="user__name">${result.name.title}  ${result.name.first}  ${result.name.last}</span> <span class="user__country">country: ${result.location.country}</span> <span class="user__e-mail">e-mail: ${result.email}</span>`;
+        usersList.appendChild(item);
+    })
+
+}
 
 document.querySelector(".button").addEventListener("click", getUsers)
